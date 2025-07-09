37e4065e4d9fa5d32135eeffea821f3e7b125f24
Amith102 <amithsuresh09@gmail.com>
2025-07-09 20:14:35 +0530

    Create treasure_island.py
    To find the treasure

---
+++ treasure_island.py
@@ -0,0 +1,42 @@
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+choice1=input('First of all which direction you want to go either "left" or "right"').lower()
+if choice1=="left":
+    choice2=input('since you have chosen to go left and now chose whether you want to "swim" or"wait"').lower()
+    if choice2=="wait":
+        choice3=input('So you\'r idea is to choose wait and now which door you want to choose ie "red" or "yellow" or "blue"' ).lower()
+        if choice3=="red":
+            print("Burned by fire Game over!!")
+        elif choice3=="blue":
+            print("Eaten by beasts And its Game over!!!")
+        elif choice3=="yellow":
+            print("You win")
+        else:
+            print("Your game is Over and you are dead")
+    else:
+        print("Attacked by trout and its Game over")
+
+else:
+    print("You fell into a hole and its game Over")
